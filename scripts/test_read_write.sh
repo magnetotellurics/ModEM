fcd81bcea86ffac9a1204f24e0fcbdd52b7f7965
protew <paulowerdt@gmail.com>
2019-06-26 16:12:07 -0300

    Create Standart Tests

---
+++ scripts/test_read_write.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# NO ARGUMENTS: 
+#
+# STRING NOW
+now=$(date "+%Y/%m/%d - %H:%M:%S")
+#
+# GET ENVIROMENT NUMBER OF CORES 
+ncores=$(nproc)
+#
+# CREATE TEST OUTPUT FOLDER
+mkdir -p test_read_write
+#
+# ENTER TEST OUTPUT FOLDER
+cd test_read_write/
+#
+#
+echo "#### START READ_WRITE MPI TEST WITH $ncores CORES AT $now ####" >> std_out.txt
+#
+#
+mpirun -n $ncores ./src/Mod3DMT ../inputs/rFile_Model.sw ../inputs/rFile_Data.dat &>> std_out.txt
+#
+#
+echo "### FINISH READ_WRITE MPI TEST ###" >> std_out.txt
+#
+#
+cd ..
+#
+#
+mv test_read_write/ outputs/
+#
+#
+# END OF SCRIPT
+
