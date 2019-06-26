fe77eceb12e97ef7eb8c2ad753d2f16589d6df08
protew <paulowerdt@gmail.com>
2019-06-26 16:47:30 -0300

    Create Standart Tests

--- scripts/test_forward.sh
+++ scripts/test_forward.sh
@@ -10,31 +10,31 @@ now=$(date "+%Y/%m/%d - %H:%M:%S")
 ncores=$(nproc)
 #
 # REMOVE TEST OUTPUT FOLDER FROM MAIN OUTPUT FOLDER
-rm -rf outputs/test_read_write/
+rm -rf outputs/test_forward/
 #
 # CREATE TEST OUTPUT FOLDER
-mkdir -p test_read_write
+mkdir -p test_forward
 #
 # ENTER TEST OUTPUT FOLDER
-cd test_read_write/
+cd test_forward/
 #
 #
-echo "#### START READ_WRITE MPI TEST WITH $ncores CORES AT $now ####\n" >> std_out.txt
+echo "#### START FORWARD MPI TEST WITH $ncores CORES AT $now ####\n" >> std_out.txt
 #
 #
 echo "#### COMMAND LINE: 'mpirun -n $ncores ../$EXEC -R ../inputs/rFile_Model.sw ../inputs/rFile_Data.dat -v full'\n" >> std_out.txt
 #
 #
-mpirun -n $ncores ../$EXEC -R ../inputs/rFile_Model.sw ../inputs/rFile_Data.dat ../inputs/wFile_Model.sw ../inputs/wFile_Data.dat -v full &>> std_out.txt
+mpirun -n $ncores ../$EXEC -F ../inputs/rFile_Model.sw ../inputs/rFile_Data.dat ../inputs/wFile_Data.dat -v full &>> std_out.txt
 #
 #
-echo "### FINISH READ_WRITE MPI TEST ###\n" >> std_out.txt
+echo "### FINISH FORWARD MPI TEST ###\n" >> std_out.txt
 #
 #
 cd ..
 #
 #
-mv test_read_write/ outputs/
+mv test_forward/ outputs/
 #
 #
 # END OF SCRIPT
