e088a26589763b5053813fe3216db70034cf7399
protew <paulowerdt@gmail.com>
2019-06-27 09:19:49 -0300

    Create Standart Tests

--- scripts/update_gitlab.sh
+++ scripts/update_gitlab.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 #
-# ARGUMENTS: 1 - JOB NAME
-job_name=$1
+# NO ARGUMENTS
 #
 # STRING NOW
 now=$(date "+%Y/%m/%d - %H:%M:%S")
 #
 #
-echo "### START UPDATE GITLAB FROM '${job_name}' AT $now ###" >> outputs/update_gitlab.txt
+echo "### START UPDATE GITLAB FROM AT $now ###"
 #
 #
-git config --global user.name "protew" &>> outputs/update_gitlab.txt
-git config --global user.email "paulowerdt@gmail.com" &>> outputs/update_gitlab.txt
-git add . &>> outputs/update_gitlab.txt
-git commit -m "GitLab Runner Push [skip ci]" &>> outputs/update_gitlab.txt
-git push https://protew:BGgwESV8qpGsBBdUZ8yk@gitlab.com/on.multiphysics/modem-on.git HEAD:master &>> outputs/update_gitlab.txt
+git config --global user.name "protew"
+git config --global user.email "paulowerdt@gmail.com"
+git add .
+git commit -m "GitLab Runner Push [skip ci]"
+git push https://protew:BGgwESV8qpGsBBdUZ8yk@gitlab.com/on.multiphysics/modem-on.git HEAD:master
 #
 #
 # END OF SCRIPT
