b75915ae959f1f63910fc0d812d646754b3f74cc
protew <paulowerdt@gmail.com>
2019-06-27 08:47:18 -0300

    Create Standart Tests [skip ci]

--- scripts/build_modem_on.sh
+++ scripts/build_modem_on.sh
@@ -12,28 +12,39 @@ mkdir -p outputs/
 cd src/
 #
 #
-echo "### START BUILD Mod3DMT QMR AT $now ###" >> build_modem.txt
+echo "### START BUILD Mod3DMT AT $now ###" #>> build_modem.txt
 #
-# DEFINE THE SELA SOLVING METHOD: QMR or BICG
-chmod 777 Configure.3D_MT.GFortran &>> build_modem.txt
+# 
+chmod 777 Configure.3D_MT.GFortran #&>> build_modem.txt
 #
 #
-./Configure.3D_MT.GFortran Makefile MPI &>> build_modem.txt
+./Configure.3D_MT.GFortran Makefile MPI OUT #&>> build_modem.txt
 #
 # REMOVE OLD OBJECTS
-rm -rf objs/3D_MT/GFortReleaseMPI/* &>> build_modem.txt
+rm -rf objs/3D_MT/GFortReleaseMPI/* #&>> build_modem.txt
 #
 # BUILD WITH PLAIN MAKE
-make &>> build_modem.txt
+make #&>> build_modem.txt
+#
+# CATCH RESULT
+result=$?
+#
+# TEST RESULT
+if [ "$result" -ne "0" ]; then
+   echo "build_modem_on FAIL: $result" #&>> build_modem.txt
+   exit $result
+fi
 #
 #
-echo "### FINISH BUILD Mod3DMT QMR ###" >> build_modem.txt
+echo "### FINISH BUILD Mod3DMT QMR ###" #>> build_modem.txt
 #
 #
-mv build_modem.txt ../outputs/
+#mv build_modem.txt ../outputs/
 #
 #
 cd ..
+#
+exit 0
 #
 #
 # END OF SCRIPT
