24f0a16197e010c91f54afb27b7dbb89a6654331
protew <paulowerdt@gmail.com>
2019-06-27 12:04:44 -0300

    Create Standart Tests

--- tests/sub01/run.sh
+++ tests/sub01/run.sh
@@ -3,6 +3,6 @@ EXEC=$1
 echo $1
 pwd
 ncores=4
-mpirun -n $ncores ../$EXEC -F ../rFile_Model.ws ../rFile_Data.dat wFile_Data.dat
+mpirun -n $ncores $EXEC -F rFile_Model.ws rFile_Data.dat wFile_Data.dat
 exit 0
 
