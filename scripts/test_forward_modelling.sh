265dff95f25a21e947dd1e6a67d93442e4555064
protew <paulowerdt@gmail.com>
2020-04-29 12:38:37 -0300

    Three main tests STD_QMR, STD_BICG and SP2_BICG

--- scripts/test_forward_modelling.sh
+++ scripts/test_forward_modelling.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #
-# ARGUMENTS: 1 - Mod3DMT PATH, 2 - USER_CTRL FILE, 3 - NUMER OF CORES
+# ARGUMENTS: 1 - Mod3DMT PATH, 2 - CTRL FILE, 3 - NUMER OF CORES
 #
-EXEC_PATH	=$1
-USER_CTRL	=$2
-ncores		=$3
+EXEC	=$1
+CTRL	=$2
+ncores	=$3
 #
-EXEC_NAME="${EXEC_PATH##*/}"
+NAME="${EXEC##*/}"
 #
 # CREATE TEST OUTPUT FOLDER
-mkdir -p test_forward_modelling_$EXEC_NAME
+mkdir -p test_forward_modelling_$NAME
 #
 # ENTER TEST OUTPUT FOLDER
-cd test_forward_modelling_$EXEC_NAME/
+cd test_forward_modelling_$NAME/
 #
 #
-echo "#### START FORWARD MODELLING $EXEC_NAME MPI TEST WITH $ncores CORES AT $now ####" | tee std_out.txt
+echo "#### START FORWARD MODELLING $NAME MPI TEST WITH $ncores CORES AT $now ####" | tee std_out.txt
 #
 #
-echo "#### COMMAND LINE: [mpirun -n $ncores ../$EXEC_PATH -W ../$USER_CTRL -v full]" | tee -a std_out.txt
+echo "#### COMMAND LINE: [mpirun -n $ncores ../$EXEC -W ../$CTRL -v full]" | tee -a std_out.txt
 #
 #
-mpirun -n $ncores ../$EXEC_PATH -W ../$USER_CTRL -v full | tee -a std_out.txt
+mpirun -n $ncores ../$EXEC -W ../$CTRL -v full | tee -a std_out.txt
 #
 # CATCH RESULT
 result=$?
@@ -30,7 +30,7 @@ result=$?
 if [ "$result" -ne "0" ]; then
 	#
 	#
-	echo "TEST FORWARD MODELLING $EXEC_NAME FAIL: $result" | tee -a std_out.txt
+	echo "TEST FORWARD MODELLING $NAME FAIL: $result" | tee -a std_out.txt
 	#
 	#
 	cd ..
@@ -40,12 +40,12 @@ if [ "$result" -ne "0" ]; then
 fi
 #
 #
-echo "#### FINISH FORWARD MODELLING $EXEC_PATH MPI TEST ####" | tee -a std_out.txt
+echo "#### FINISH FORWARD MODELLING $EXEC MPI TEST ####" | tee -a std_out.txt
 #
 #
 cd ..
 #
-mv test_forward_modelling_$EXEC_NAME/ outputs/temp/
+mv test_forward_modelling_$NAME/ outputs/temp/
 #
 #
 exit 0
