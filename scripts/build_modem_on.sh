60d87146b097e464a1b6bdfaf3470c1949fc408e
protew <paulowerdt@gmail.com>
2020-08-17 11:36:13 -0300

    Cleanning scripts and exe

--- scripts/build_modem_on.sh
+++ scripts/build_modem_on.sh
@@ -5,9 +5,11 @@
 # STRING NOW
 now=$(date "+%Y/%m/%d - %H:%M:%S")
 #
-#
 cd src/
 #
+########################
+# STANDART VERSION
+########################
 #
 echo "#### START BUILD STANDART Mod3DMT AT $now ####"
 #
@@ -26,6 +28,27 @@ make -f Makefile_STD
 # RENAME EXE
 mv Mod3DMT ../bin/Mod3DMT_STD
 #
+########################
+# SP2 VERSION
+########################
+#
+echo "#### START BUILD SP2 Mod3DMT AT $now ####"
+#
+# GRANT PERMISSION TO Configure.3D_MT_SP2.OSU.GFortran
+chmod 777 CONFIG/Configure.3D_MT_SP2.OSU.GFortran
+#
+# CREATE Makefile_STD
+./CONFIG/Configure.3D_MT_SP2.OSU.GFortran Makefile_SP2 MPI
+#
+# REMOVE OLD OBJECTS
+rm -rf ../../objs/
+#
+# BUILD WITH PLAIN MAKE
+make -f Makefile_SP2
+#
+# RENAME EXE
+mv Mod3DMT ../bin/Mod3DMT_SP2
+#
 # CATCH RESULT
 result=$?
 #
@@ -33,7 +56,7 @@ result=$?
 if [ "$result" -ne "0" ]; then
 	#
 	#
-	echo "BUILD Mod3DMT_STD FAIL: $result"
+	echo "build_modem_on FAIL: $result"
 	#
 	#
 	cd ..
@@ -43,7 +66,7 @@ if [ "$result" -ne "0" ]; then
 fi
 #
 #
-echo "#### FINISH BUILD Mod3DMT_STD QMR ####"
+echo "#### FINISH BUILD Mod3DMT QMR ####"
 #
 #
 cd ..
