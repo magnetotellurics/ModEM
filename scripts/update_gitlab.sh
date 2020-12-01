526d08bc598e4807fa00dfd35a349c13a36a4182
protew <paulowerdt@gmail.com>
2020-12-01 15:55:39 -0300

    Setup branch for production

--- scripts/update_gitlab.sh
+++ scripts/update_gitlab.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 #
-# NO ARGUMENTS
+# ARGUMENTS: 1 - BRANCH NAME
 #
+#
+branch_name=$1
 # STRING NOW
 now=$(date "+%Y/%m/%d - %H:%M:%S")
 #
 #
-echo "#### START UPDATE GITLAB FROM AT $now ####"
+echo "#### START UPDATE $branch_name FROM AT $now ####"
 #
 #
 git config --global user.name "protew"
 git config --global user.email "paulowerdt@gmail.com"
 git add .
-git commit -m "GitLab Runner Push [skip ci]"
-git push https://protew:BGgwESV8qpGsBBdUZ8yk@gitlab.com/on.multiphysics/modem-on.git HEAD:master
+git commit -m "GitLab '$branch_name' Runner Push [skip ci]"
+git push https://protew:BGgwESV8qpGsBBdUZ8yk@gitlab.com/on.multiphysics/modem-on.git HEAD:$branch_name
 #
 # CATCH RESULT
 result=$?
@@ -30,6 +32,7 @@ fi
 #
 #
 echo "#### FINISH UPDATE GITLAB FROM AT $now ####"
+#
 #
 exit 0
 #
