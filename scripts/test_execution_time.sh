beea2710be1f5b362fa00dc1f23fadf6099ec83e
protew <paulowerdt@on.br>
2022-04-08 10:25:59 -0300

    Cleaning and test

--- scripts/test_execution_time.sh
+++ scripts/test_execution_time.sh
@@ -9,4 +9,9 @@ make -f MakefileSerial
 ./Configure.3D_MT.OSU.GFortran MakefileMPI MPI test_FWD_MPI.f90
 make -f MakefileMPI clean
 make -f MakefileMPI
-mpirun -np 5 ./test_FWD_MPI -f --data ../inputs/esol/de.dat --model ../inputs/simple_2_blocks.cpr --control ../inputs/first_control_file.txt
+mpirun -np 4 ./test_FWD_MPI -f --data ../inputs/esol/de.dat --model ../inputs/simple_2_blocks.cpr --control ../inputs/first_control_file.txt
+
+-F  rFile_Model rFile_Data wFile_Data
+
+
+mpirun -np 5 ./Mod3DMT_SP2 ../inputs/simple_2_blocks.cpr ../inputs/esol/de.dat predicted_data_base.dat
