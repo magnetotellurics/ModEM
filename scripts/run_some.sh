998d3b4bc3928440be243f3a83371c9e1d28dca7
protew <paulowerdt@gmail.com>
2022-07-01 11:26:18 -0300

    Tests

--- scripts/run_some.sh
+++ scripts/run_some.sh
@@ -1,124 +1,179 @@
 #!/bin/bash
 #
-echo "MAIN SCRIPT STARTS: $(date "+%Y/%m/%d - %H:%M:%S")" | tee -a summary.txt
+OUTPUT_FOLDER=$1
 #
 T_START=$(date +%s%3N)
 #
-# FWD
+# FWD MT
 #
-mkdir TEST_MT_VALGRIND_FWD
-	#
-	cd TEST_MT_VALGRIND_FWD
-		#
-		mpirun -np 9  valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=fwd_valgrind_mt.txt ../Mod3DMT -F ../../inputs/Benchmarking/rFile_Model ../../inputs/Benchmarking/rFile_Data predicted
-		#
-		T_END=$(date +%s%3N)
-		echo "	> TEST_MT_VALGRIND_FWD: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
-		T_START=$(date +%s%3N)
-		#
-	cd ..
-	#
-mkdir TEST_CSEM_VALGRIND_FWD
-	#
-	cd TEST_CSEM_VALGRIND_FWD
-		#
-		mpirun -np 46  valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=fwd_valgrind_csem.txt ../Mod3DMT -F ../../inputs/Naser_Zap/Start_model_WithGrad.mod  ../../inputs/Naser_Zap/Field_CSEM_data20%err.dat predicted
-		#
-		T_END=$(date +%s%3N)
-		echo "	> TEST_CSEM_VALGRIND_FWD: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
-		T_START=$(date +%s%3N)
-		#
-	cd ..
-	#
-	#
-mkdir TEST_MT_MASSIF_FWD
-	#
-	cd TEST_MT_MASSIF_FWD
-		#
-		mpirun -np 9 valgrind --tool=massif --time-unit=ms --max-snapshots=1000 ../Mod3DMT -F ../../inputs/Benchmarking/rFile_Model ../../inputs/Benchmarking/rFile_Data predicted
-		#
-		bash ../../scripts/memory_brief.sh
-		#
-		T_END=$(date +%s%3N)
-		echo "	> TEST_MT_MASSIF_FWD: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
-		T_START=$(date +%s%3N)
-		#
-	cd ..
-	#
-mkdir TEST_CSEM_MASSIF_FWD
-	#
-	cd TEST_CSEM_MASSIF_FWD
-		#
-		mpirun -np 46 valgrind --tool=massif --time-unit=ms --max-snapshots=1000 ../Mod3DMT -F ../../inputs/Naser_Zap/Start_model_WithGrad.mod  ../../inputs/Naser_Zap/Field_CSEM_data20%err.dat predicted
-		#
-		bash ../../scripts/memory_brief.sh
-		#
-		T_END=$(date +%s%3N)
-		echo "	> TEST_CSEM_MASSIF_FWD: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
-		T_START=$(date +%s%3N)
-		#
-	cd ..
-	#
-	#
-#
-# INV
-#
-mkdir TEST_MT_VALGRIND_INV
-	#
-	cd TEST_MT_VALGRIND_INV
-		#
-		mpirun -np 9  valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=inv_valgrind_mt.txt ../Mod3DMT -I NLCG ../../inputs/Benchmarking/rFile_Model ../../inputs/Benchmarking/rFile_Data
-		#
-		T_END=$(date +%s%3N)
-		echo "	> TEST_MT_VALGRIND_INV: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
-		T_START=$(date +%s%3N)
-		#
-	cd ..
-	#
-	#
-mkdir TEST_CSEM_VALGRIND_INV
-	#
-	cd TEST_CSEM_VALGRIND_INV
-		#
-		mpirun -np 46  valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=inv_valgrind_csem.txt ../Mod3DMT -I NLCG ../../inputs/Naser_Zap/Start_model_WithGrad.mod ../../inputs/Naser_Zap/Field_CSEM_data20%err.dat ../../inputs/Naser_Zap/Inv_para.dat ../../inputs/Naser_Zap/FWD_para.dat ../../inputs/Naser_Zap/Start_model_WithGrad.cov
-		#
-		T_END=$(date +%s%3N)
-		echo "	> TEST_CSEM_VALGRIND_INV: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
-		T_START=$(date +%s%3N)
-		#
-	cd ..
-	#
-	#
-mkdir TEST_MT_MASSIF_INV
-	#
-	cd TEST_MT_MASSIF_INV
-		#
-		mpirun -np 9 valgrind --tool=massif --time-unit=ms --max-snapshots=1000 ../Mod3DMT -I NLCG ../../inputs/Benchmarking/rFile_Model ../../inputs/Benchmarking/rFile_Data
-		#
-		bash ../../scripts/memory_brief.sh
-		#
-		T_END=$(date +%s%3N)
-		echo "	> TEST_MT_MASSIF_INV: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
-		T_START=$(date +%s%3N)
-		#
-	cd ..
-	#
-mkdir TEST_CSEM_MASSIF_INV
-	#
-	cd TEST_CSEM_MASSIF_INV
-	#
-	mpirun -np 46 valgrind --tool=massif --time-unit=ms --max-snapshots=1000 ../Mod3DMT -I NLCG ../../inputs/Naser_Zap/Start_model_WithGrad.mod ../../inputs/Naser_Zap/Field_CSEM_data20%err.dat ../../inputs/Naser_Zap/Inv_para.dat ../../inputs/Naser_Zap/FWD_para.dat ../../inputs/Naser_Zap/Start_model_WithGrad.cov
-	#
-	bash ../../scripts/memory_brief.sh
-	#
-	T_END=$(date +%s%3N)
-	echo "	> TEST_CSEM_MASSIF_INV: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
-	#
-	cd ..
-	#
-#
-echo "MAIN SCRIPT FINISHES: $(date "+%Y/%m/%d - %H:%M:%S")" | tee -a summary.txt
-#
+mkdir $OUTPUT_FOLDER
+cd $OUTPUT_FOLDER
+    #
+    echo "MAIN SCRIPT STARTS: $(date "+%Y/%m/%d - %H:%M:%S")" | tee -a summary.txt
+    #
+    mkdir TEST_MT_FWD_MPI
+        #
+        cd TEST_MT_FWD_MPI
+            #
+            mpirun -np 2 ../../Mod3DMT -F ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_MT wFile_Data wFile_EMsoln ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_MT_FWD_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+        #
+    mkdir TEST_MT_VALGRIND_FWD_MPI
+        #
+        cd TEST_MT_VALGRIND_FWD_MPI
+            #
+            mpirun -np 2 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --suppressions=../../../../MEETING_INPUTS/valgring_mpi_suppression.supp --log-file=mt_valgrind_fwd_mpi.txt ../../Mod3DMT -F ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_MT wFile_Data wFile_EMsoln ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_MT_VALGRIND_FWD_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+        #
+    mkdir TEST_MT_MASSIF_FWD_MPI
+        #
+        cd TEST_MT_MASSIF_FWD_MPI
+            #
+            mpirun -np 2 valgrind --tool=massif --time-unit=ms --max-snapshots=1000 ../../Mod3DMT -F ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_MT wFile_Data wFile_EMsoln ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            bash ../../scripts/memory_brief.sh
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_MT_MASSIF_FWD_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+    #
+    # FWD CSEM
+    #
+    mkdir TEST_CSEM_FWD_MPI
+        #
+        cd TEST_CSEM_FWD_MPI
+            #
+            mpirun -np 2 ../../Mod3DMT -F ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_CSEM wFile_Data wFile_EMsoln ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_CSEM_FWD_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+        #
+    mkdir TEST_CSEM_VALGRIND_FWD_MPI
+        #
+        cd TEST_CSEM_VALGRIND_FWD_MPI
+            #
+            mpirun -np 2 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --suppressions=../../../../MEETING_INPUTS/valgring_mpi_suppression.supp --log-file=mt_valgrind_fwd_mpi.txt ../../Mod3DMT -F ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_CSEM wFile_Data wFile_EMsoln ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_CSEM_VALGRIND_FWD_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+        #
+    mkdir TEST_CSEM_MASSIF_FWD_MPI
+        #
+        cd TEST_CSEM_MASSIF_FWD_MPI
+            #
+            mpirun -np 2 valgrind --tool=massif --time-unit=ms --max-snapshots=1000 ../../Mod3DMT -F ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_CSEM wFile_Data wFile_EMsoln ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            bash ../../scripts/memory_brief.sh
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_CSEM_MASSIF_FWD_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+        #
+    echo "FWD FINISHES: $(date "+%Y/%m/%d - %H:%M:%S")" | tee -a summary.txt
+    #
+    # INV MT
+    #
+    mkdir TEST_MT_INV_MPI
+        #
+        cd TEST_MT_INV_MPI
+            #
+            mpirun -np 2 ../../Mod3DMT -I NLCG ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_MT ../../../../MEETING_INPUTS/rFile_invCtrl ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_MT_INV_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+        #
+    mkdir TEST_MT_VALGRIND_INV_MPI
+        #
+        cd TEST_MT_VALGRIND_INV_MPI
+            #
+            mpirun -np 2 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --suppressions=../../../../MEETING_INPUTS/valgring_mpi_suppression.supp --log-file=mt_valgrind_fwd_mpi.txt ../../Mod3DMT -I NLCG ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_MT ../../../../MEETING_INPUTS/rFile_invCtrl ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_MT_VALGRIND_INV_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+        #
+    mkdir TEST_MT_MASSIF_INV_MPI
+        #
+        cd TEST_MT_MASSIF_INV_MPI
+            #
+            mpirun -np 2 valgrind --tool=massif --time-unit=ms --max-snapshots=1000 ../../Mod3DMT -I NLCG ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_MT ../../../../MEETING_INPUTS/rFile_invCtrl ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            bash ../../scripts/memory_brief.sh
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_MT_MASSIF_INV_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+    #
+    # INV CSEM
+    #
+    mkdir TEST_CSEM_INV_MPI
+        #
+        cd TEST_CSEM_INV_MPI
+            #
+            mpirun -np 2 ../../Mod3DMT -I NLCG ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_CSEM ../../../../MEETING_INPUTS/rFile_invCtrl ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_CSEM_INV_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+        #
+    mkdir TEST_CSEM_VALGRIND_INV_MPI
+        #
+        cd TEST_CSEM_VALGRIND_INV_MPI
+            #
+            mpirun -np 2 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --suppressions=../../../../MEETING_INPUTS/valgring_mpi_suppression.supp --log-file=mt_valgrind_fwd_mpi.txt ../../Mod3DMT -I NLCG ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_CSEM ../../../../MEETING_INPUTS/rFile_invCtrl ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_CSEM_VALGRIND_INV_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+        #
+    mkdir TEST_CSEM_MASSIF_INV_MPI
+        #
+        cd TEST_CSEM_MASSIF_INV_MPI
+            #
+            mpirun -np 2 valgrind --tool=massif --time-unit=ms --max-snapshots=1000 ../../Mod3DMT -I NLCG ../../../../MEETING_INPUTS/rFile_Model ../../../../MEETING_INPUTS/rFile_Data_CSEM ../../../../MEETING_INPUTS/rFile_invCtrl ../../../../MEETING_INPUTS/rFile_fwdCtrl | tee -a output.txt
+            #
+            bash ../../scripts/memory_brief.sh
+            #
+            T_END=$(date +%s%3N)
+            echo "    > TEST_CSEM_MASSIF_INV_MPI: $(( ( $T_END - $T_START ) / 1000 )) seconds" | tee -a ../summary.txt
+            T_START=$(date +%s%3N)
+            #
+        cd ..
+        #
+    echo "MAIN SCRIPT FINISHES: $(date "+%Y/%m/%d - %H:%M:%S")" | tee -a summary.txt
+    #
+cd ..
 exit 0
 #
 # END OF SCRIPT
