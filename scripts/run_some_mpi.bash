6b6724af25b85e5def32cd6c45ee8a4761cc04be
protew <paulowerdt@gmail.com>
2022-07-12 07:03:27 -0300

    Working merge

--- scripts/run_some_mpi.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#--suppressions=../../../MEETING_INPUTS/modem_oo_suppresions.supp
-#
-mpirun -np 2 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --log-file=oo_mt_valgrind_fwd_mpi.txt ../test_FWD_MPI --forward --model ../../../MEETING_INPUTS/rFile_Model  --data ../../../MEETING_INPUTS/rFile_Data_MT --control ../../inputs/Others/first_control_file.txt | tee -a mt_output.txt
-#
-mpirun -np 2 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --log-file=oo_csem_valgrind_fwd_mpi.txt ../test_FWD_MPI --forward --model ../../../MEETING_INPUTS/rFile_Model  --data ../../../MEETING_INPUTS/rFile_Data_CSEM --control ../../inputs/Others/first_control_file.txt | tee -a output.txt
-#
