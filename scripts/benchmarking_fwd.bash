12ad0cc18df4495b99f37d73aacac3056365d49a
protew <paulowerdt@gmail.com>
2023-06-29 13:31:21 -0700

    VTI MT/Dipole1D/EM1D FWD - Massive Urca Tests

--- scripts/benchmarking_fwd.bash
+++ scripts/benchmarking_fwd.bash
@@ -1,56 +1,68 @@
 #!/bin/bash
 #
-# OO FWD
+# OO
 #
-# SERIAL
+# FWD
 #
-#./ModEM_SERIAL -f -m ../../results/model_ISO_h10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -pd pred_mt1_iso_oo_serial.dat
+mpirun -np 2 ./ModEM_MPI -f -m ../docs/vti_results/model_ISO_h10.mod -d ../docs/vti_results/Data_Z_1tx_1rx.dat -pd pred_mt_iso_oo.dat -es esol_mt_iso_oo.bin -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-#./ModEM_SERIAL -f -m ../../results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -pd pred_mt1_vti_oo_serial.dat
+mpirun -np 2 ./ModEM_MPI -f -m ../docs/vti_results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../docs/vti_results/Data_Z_1tx_1rx.dat -pd pred_mt_vti_oo.dat -es esol_mt_vti_oo.bin -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-#./ModEM_SERIAL -f -m ../../results/model_ISO_h10.mod -d ../../results/Data_Ey.dat -cf fwd_ctrl_dip1d -pd pred_dip1d_oo_serial.dat
+mpirun -np 2 ./ModEM_MPI -f -m ../docs/vti_results/model_ISO_h10.mod -d ../docs/vti_results/Data_Ey_1tx_1rx.dat -pd pred_dip1d_oo.dat -es esol_dip1d_oo.bin -cf ../docs/vti_results/fwd_ctrl_dip1d
 #
-#./ModEM_SERIAL -f -m ../../results/model_ISO_h10.mod -d ../../results/Data_Ey.dat -cf fwd_ctrl_em1d -pd pred_em1d_iso_oo_serial.dat
+mpirun -np 2 ./ModEM_MPI -f -m ../docs/vti_results/model_ISO_h10.mod -d ../docs/vti_results/Data_Ey_1tx_1rx.dat -pd pred_em1d_iso_oo.dat -es esol_em1d_iso_oo.bin -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-#./ModEM_SERIAL -f -m ../../results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../../results/Data_Ey.dat -cf fwd_ctrl_em1d -pd pred_em1d_vti_oo_serial.dat
+mpirun -np 2 ./ModEM_MPI -f -m ../docs/vti_results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../docs/vti_results/Data_Ey_1tx_1rx.dat -pd pred_em1d_vti_oo.dat -es esol_em1d_vti_oo.bin -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-# MPI
+# JMULT
 #
-mpirun -np 22 ./ModEM_MPI -f -m ../../results/model_ISO_h10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -pd pred_mt_iso_oo_mpi.dat
+mpirun -np 2 ./ModEM_MPI -j -m ../docs/vti_results/model_ISO_h0.mod -pm ../docs/vti_results/model_ISO_h10.mod -d ../docs/vti_results/Data_Z_1tx_1rx.dat -jm jmhat_mt_iso_oo.dat -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-mpirun -np 22 ./ModEM_MPI -f -m ../../results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -pd pred_mt_vti_oo_mpi.dat
+mpirun -np 2 ./ModEM_MPI -j -m ../docs/vti_results/model_VTI_h0_v0.mod -pm ../docs/vti_results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../docs/vti_results/Data_Z_1tx_1rx.dat -jm jmhat_mt_vti_oo.dat -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-mpirun -np 22 ./ModEM_MPI -f -m ../../results/model_ISO_h10.mod -d ../../results/Data_Ey.dat -cf fwd_ctrl_dip1d -pd pred_dip1d_oo_mpi.dat
+mpirun -np 2 ./ModEM_MPI -j -m ../docs/vti_results/model_ISO_h0.mod -pm ../docs/vti_results/model_ISO_h10.mod -d ../docs/vti_results/Data_Ey_1tx_1rx.dat -jm jmhat_dip1d_oo.dat -cf ../docs/vti_results/fwd_ctrl_dip1d
 #
-mpirun -np 22 ./ModEM_MPI -f -m ../../results/model_ISO_h10.mod -d ../../results/Data_Ey.dat -cf fwd_ctrl_em1d -pd pred_em1d_iso_oo_mpi.dat
+mpirun -np 2 ./ModEM_MPI -j -m ../docs/vti_results/model_ISO_h0.mod -pm ../docs/vti_results/model_ISO_h10.mod -d ../docs/vti_results/Data_Ey_1tx_1rx.dat -jm jmhat_em1d_iso_oo.dat -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-mpirun -np 22 ./ModEM_MPI -f -m ../../results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../../results/Data_Ey.dat -cf fwd_ctrl_em1d -pd pred_em1d_vti_oo_mpi.dat
+mpirun -np 2 ./ModEM_MPI -j -m ../docs/vti_results/model_VTI_h0_v0.mod -pm ../docs/vti_results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../docs/vti_results/Data_Ey_1tx_1rx.dat -jm jmhat_em1d_vti_oo.dat -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-mpirun -np 22 ./ModEM_MPI -j -m ../../results/model_ISO_h0.mod -pm ../../results/model_ISO_h10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -jm jmhat_mt_iso_oo_mpi.dat
+# JMULT
 #
-mpirun -np 22 ./ModEM_MPI -j -m ../../results/model_ISO_h0.mod -pm ../../results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -jm jmhat_mt_vti_oo_mpi.dat
+mpirun -np 2 ./ModEM_MPI -jt -m ../docs/vti_results/model_ISO_h0.mod -d pred_mt_iso_oo.dat -dm dsigma_mt_iso_oo.rho -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-mpirun -np 22 ./ModEM_MPI -jt -m ../../results/model_ISO_h10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -dm sigma_mt_iso_oo_mpi.rho
+mpirun -np 2 ./ModEM_MPI -jt -m ../docs/vti_results/model_VTI_h0_v0.mod -d pred_mt_vti_oo.dat -dm dsigma_mt_vti_oo.rho -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-mpirun -np 22 ./ModEM_MPI -jt -m ../../results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -dm sigma_mt_vti_oo_mpi.rho
+mpirun -np 2 ./ModEM_MPI -jt -m ../docs/vti_results/model_ISO_h0.mod -d pred_dip1d_oo.dat -dm dsigma_dip1d_oo.rho -cf ../docs/vti_results/fwd_ctrl_dip1d
 #
-mpirun -np 22 ./ModEM_MPI -i -m ../../results/model_ISO_h10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -ci inv_contrl_nlcg.txt -o nlcg_mt_iso_oo_mpi
+mpirun -np 2 ./ModEM_MPI -jt -m ../docs/vti_results/model_ISO_h0.mod -d pred_em1d_iso_oo.dat -dm dsigma_em1d_iso_oo.rho -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-mpirun -np 22 ./ModEM_MPI -i -m ../../results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -ci inv_contrl_nlcg.txt -o nlcg_mt_vti_oo_mpi
+mpirun -np 2 ./ModEM_MPI -jt -m ../docs/vti_results/model_VTI_h0_v0.mod -d pred_em1d_vti_oo.dat -dm dsigma_em1d_vti_oo.rho -cf ../docs/vti_results/fwd_ctrl_em1d
 #
-mpirun -np 22 ./ModEM_MPI -i -m ../../results/model_ISO_h10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -ci inv_contrl_dcg.txt -o nlcg_mt_iso_oo_mpi
+# NLCG
 #
-mpirun -np 22 ./ModEM_MPI -i -m ../../results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -ci inv_contrl_dcg.txt -o nlcg_mt_vti_oo_mpi
+mpirun -np 2 ./ModEM_MPI -i -m ../docs/vti_results/model_ISO_h0.mod -d pred_mt_iso_oo.dat -ci inv_contrl_nlcg.txt -cf ../docs/vti_results/fwd_ctrl_em1d -o mt_iso_oo_nlcg
+#
+mpirun -np 2 ./ModEM_MPI -i -m ../docs/vti_results/model_VTI_h0_v0.mod -d pred_mt_vti_oo.dat -ci inv_contrl_nlcg.txt -cf ../docs/vti_results/fwd_ctrl_em1d -o mt_vti_oo_nlcg
+#
+mpirun -np 2 ./ModEM_MPI -i -m ../docs/vti_results/model_ISO_h0.mod -d pred_dip1d_oo.dat -ci inv_contrl_nlcg.txt -cf ../docs/vti_results/fwd_ctrl_dip1d -o dip1d_oo_nlcg
+#
+mpirun -np 2 ./ModEM_MPI -i -m ../docs/vti_results/model_ISO_h0.mod -d pred_em1d_iso_oo.dat -ci inv_contrl_nlcg.txt -cf ../docs/vti_results/fwd_ctrl_em1d -o em1d_iso_oo_nlcg
+#
+mpirun -np 2 ./ModEM_MPI -i -m ../docs/vti_results/model_VTI_h0_v0.mod -d pred_em1d_vti_oo.dat -ci inv_contrl_nlcg.txt -cf ../docs/vti_results/fwd_ctrl_em1d -o em1d_vti_oo_nlcg
+#
+# DCG
+#
+mpirun -np 2 ./ModEM_MPI -i -m ../docs/vti_results/model_ISO_h0.mod -d pred_mt_iso_oo.dat -ci inv_contrl_dcg.txt -cf ../docs/vti_results/fwd_ctrl_em1d -o mt_iso_oo_nlcg
+#
+mpirun -np 2 ./ModEM_MPI -i -m ../docs/vti_results/model_VTI_h0_v0.mod -d pred_mt_vti_oo.dat -ci inv_contrl_dcg.txt -cf ../docs/vti_results/fwd_ctrl_em1d -o mt_vti_oo_nlcg
+#
+mpirun -np 2 ./ModEM_MPI -i -m ../docs/vti_results/model_ISO_h0.mod -d pred_dip1d_oo.dat -ci inv_contrl_dcg.txt -cf ../docs/vti_results/fwd_ctrl_dip1d -o dip1d_oo_nlcg
+#
+mpirun -np 2 ./ModEM_MPI -i -m ../docs/vti_results/model_ISO_h0.mod -d pred_em1d_iso_oo.dat -ci inv_contrl_dcg.txt -cf ../docs/vti_results/fwd_ctrl_em1d -o em1d_iso_oo_nlcg
+#
+mpirun -np 2 ./ModEM_MPI -i -m ../docs/vti_results/model_VTI_h0_v0.mod -d pred_em1d_vti_oo.dat -ci inv_contrl_dcg.txt -cf ../docs/vti_results/fwd_ctrl_em1d -o em1d_vti_oo_nlcg
 #
 # MPI VALGRIND
 #
-#mpirun -np 22 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --log-file=valgrind_pred_mt_iso_oo_mpi.txt ./ModEM_MPI -f -m ../../results/model_ISO_h10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -pd pred_mt_iso_oo_mpi.dat
-#
-#mpirun -np 22 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --log-file=valgrind_pred_mt_vti_oo_mpi.txt ./ModEM_MPI -f -m ../../results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../../results/Data_Z.dat -cf fwd_ctrl_em1d -pd pred_mt_vti_oo_mpi.dat
-#
-#mpirun -np 22 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --log-file=valgrind_pred_dip1d_oo_mpi.txt ./ModEM_MPI -f -m ../../results/model_ISO_h10.mod -d ../../results/Data_Ey.dat -cf fwd_ctrl_dip1d -pd pred_dip1d_oo_mpi.dat
-#
-#mpirun -np 22 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --log-file=valgrind_pred_em1d_iso_oo_mpi.txt ./ModEM_MPI -f -m ../../results/model_ISO_h10.mod -d ../../results/Data_Ey.dat -cf fwd_ctrl_em1d -pd pred_em1d_iso_oo_mpi.dat
-#
-#mpirun -np 22 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --log-file=valgrind_pred_em1d_vti_oo_mpi.txt ./ModEM_MPI -f -m ../../results/VTI_H10_V10/model_VTI_h10_v10.mod -d ../../results/Data_Ey.dat -cf fwd_ctrl_em1d -pd pred_em1d_vti_oo_mpi.dat
+#mpirun -np 2 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --log-file=valgrind_pred_mt_iso_oo.txt ./ModEM_MPI -f -m ../docs/vti_results/model_ISO_h10.mod -d ../docs/vti_results/Data_Z_1tx_1rx.dat -cf ../docs/vti_results/fwd_ctrl_em1d -pd pred_mt_iso_oo.dat
 #
---
+++ scripts/benchmarking_fwd_on.bash
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# OO
+#
+# FWD
+#
+mpirun -np 22 ./Mod3DMT_STD -F ../docs/vti_results/model_ISO_h10.mod ../docs/vti_results/Data_Z.dat pred_mt_iso_on.dat esol_mt_iso_on.bin ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+mpirun -np 22 ./Mod3DMT_STD -F ../docs/vti_results/VTI_H10_V10/model_VTI_h10_v10.mod ../docs/vti_results/Data_Z.dat pred_mt_vti_on.dat esol_mt_vti_on.bin ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+mpirun -np 22 ./Mod3DMT_STD -F ../docs/vti_results/model_ISO_h10.mod ../docs/vti_results/Data_Ey.dat pred_dip1d_on.dat esol_dip1d_on.bin ../../modem-oo/docs/vti_results/on_fwd_control_dip1d
+#
+mpirun -np 22 ./Mod3DMT_STD -F ../docs/vti_results/model_ISO_h10.mod ../docs/vti_results/Data_Ey.dat pred_em1d_iso_on.dat esol_em1d_iso_on.bin ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+mpirun -np 22 ./Mod3DMT_STD -F ../docs/vti_results/VTI_H10_V10/model_VTI_h10_v10.mod ../docs/vti_results/Data_Ey.dat pred_em1d_vti_on.dat esol_em1d_vti_on.bin ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+# JMULT
+#
+mpirun -np 22 ./Mod3DMT_STD -M ../docs/vti_results/model_ISO_h0.mod ../docs/vti_results/model_ISO_h10.mod ../docs/vti_results/Data_Z.dat jmhat_mt_iso_on.dat ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+mpirun -np 22 ./Mod3DMT_STD -M ../docs/vti_results/model_VTI_h0_v0.mod ../docs/vti_results/VTI_H10_V10/model_VTI_h10_v10.mod ../docs/vti_results/Data_Z.dat jmhat_mt_vti_on.dat ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+mpirun -np 22 ./Mod3DMT_STD -M ../docs/vti_results/model_ISO_h0.mod ../docs/vti_results/model_ISO_h10.mod ../docs/vti_results/Data_Ey.dat jmhat_dip1d_on.dat ../../modem-oo/docs/vti_results/on_fwd_control_dip1d
+#
+mpirun -np 22 ./Mod3DMT_STD -M ../docs/vti_results/model_ISO_h0.mod ../docs/vti_results/model_ISO_h10.mod ../docs/vti_results/Data_Ey.dat jmhat_em1d_iso_on.dat ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+mpirun -np 22 ./Mod3DMT_STD -M ../docs/vti_results/model_VTI_h0_v0.mod ../docs/vti_results/VTI_H10_V10/model_VTI_h10_v10.mod ../docs/vti_results/Data_Ey.dat jmhat_em1d_vti_on.dat ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+# JMULT
+#
+mpirun -np 22 ./Mod3DMT_STD -T ../docs/vti_results/model_ISO_h0.mod pred_mt_iso_on.dat dsigma_mt_iso_on.rho ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+mpirun -np 22 ./Mod3DMT_STD -T ../docs/vti_results/model_VTI_h0_v0.mod pred_mt_vti_on.dat dsigma_mt_vti_on.rho ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+mpirun -np 22 ./Mod3DMT_STD -T ../docs/vti_results/model_ISO_h0.mod pred_dip1d_on.dat dsigma_dip1d_on.rho ../../modem-oo/docs/vti_results/on_fwd_control_dip1d
+#
+mpirun -np 22 ./Mod3DMT_STD -T ../docs/vti_results/model_ISO_h0.mod pred_em1d_iso_on.dat dsigma_em1d_iso_on.rho ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+mpirun -np 22 ./Mod3DMT_STD -T ../docs/vti_results/model_VTI_h0_v0.mod pred_em1d_vti_on.dat dsigma_em1d_vti_on.rho ../../modem-oo/docs/vti_results/on_fwd_control_em1d
+#
+# NLCG
+#
+mpirun -np 22 ./Mod3DMT_STD -I NLCG ../docs/vti_results/model_ISO_h0.mod pred_mt_iso_on.dat ../../modem-oo/docs/vti_results/on_inv_control_iso ../../modem-oo/docs/vti_results/on_fwd_control_em1d -o mt_iso_on_nlcg
+#
+mpirun -np 22 ./Mod3DMT_STD -I NLCG ../docs/vti_results/model_VTI_h0_v0.mod pred_mt_vti_on.dat ../../modem-oo/docs/vti_results/on_inv_control_vti ../../modem-oo/docs/vti_results/on_fwd_control_em1d -o mt_vti_on_nlcg
+#
+mpirun -np 22 ./Mod3DMT_STD -I NLCG ../docs/vti_results/model_ISO_h0.mod pred_dip1d_on.dat ../../modem-oo/docs/vti_results/on_inv_control_iso ../../modem-oo/docs/vti_results/on_fwd_control_dip1d -o dip1d_on_nlcg
+#
+mpirun -np 22 ./Mod3DMT_STD -I NLCG ../docs/vti_results/model_ISO_h0.mod pred_em1d_iso_on.dat ../../modem-oo/docs/vti_results/on_inv_control_iso ../../modem-oo/docs/vti_results/on_fwd_control_em1d -o em1d_iso_on_nlcg
+#
+mpirun -np 22 ./Mod3DMT_STD -I NLCG ../docs/vti_results/model_VTI_h0_v0.mod pred_em1d_vti_on.dat ../../modem-oo/docs/vti_results/on_inv_control_vti ../../modem-oo/docs/vti_results/on_fwd_control_em1d -o em1d_vti_on_nlcg
+#
+# DCG
+#
+mpirun -np 22 ./Mod3DMT_STD -I DCG ../docs/vti_results/model_ISO_h0.mod pred_mt_iso_on.dat ../../modem-oo/docs/vti_results/on_inv_control_iso ../../modem-oo/docs/vti_results/on_fwd_control_em1d -o mt_iso_on_nlcg
+#
+mpirun -np 22 ./Mod3DMT_STD -I DCG ../docs/vti_results/model_VTI_h0_v0.mod pred_mt_vti_on.dat ../../modem-oo/docs/vti_results/on_inv_control_vti ../../modem-oo/docs/vti_results/on_fwd_control_em1d -o mt_vti_on_nlcg
+#
+mpirun -np 22 ./Mod3DMT_STD -I DCG ../docs/vti_results/model_ISO_h0.mod pred_dip1d_on.dat ../../modem-oo/docs/vti_results/on_inv_control_iso ../../modem-oo/docs/vti_results/on_fwd_control_dip1d -o dip1d_on_nlcg
+#
+mpirun -np 22 ./Mod3DMT_STD -I DCG ../docs/vti_results/model_ISO_h0.mod pred_em1d_iso_on.dat ../../modem-oo/docs/vti_results/on_inv_control_iso ../../modem-oo/docs/vti_results/on_fwd_control_em1d -o em1d_iso_on_nlcg
+#
+mpirun -np 22 ./Mod3DMT_STD -I DCG ../docs/vti_results/model_VTI_h0_v0.mod pred_em1d_vti_on.dat ../../modem-oo/docs/vti_results/on_inv_control_vti ../../modem-oo/docs/vti_results/on_fwd_control_em1d -o em1d_vti_on_nlcg
+#
+# MPI VALGRIND
+#
+#mpirun -np 22 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --log-file=valgrind_pred_mt_iso_on.txt ./Mod3DMT_STD -F ../docs/vti_results/model_ISO_h10.mod ../docs/vti_results/Data_Z.dat ../../modem-oo/docs/vti_results/on_fwd_control_em1d pred_mt_iso_on.dat
+#
