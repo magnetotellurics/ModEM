8d2c7a3610e41a1606f0ef547c5dbf2577552498
protew <paulowerdt@gmail.com>
2023-07-07 13:56:24 -0700

    CSEM VTI Inversion tests

---
+++ src/save_results.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+mkdir -p $1/
+mv *_NLCG_* $1/
+mv Nodes_Status* $1/
+mv QMR_SolverStatFile* $1/
+mv *.rho $1/
+mv *.vg $1/
+mv fort.* $1/
+mv *.dat* $1/
+mv esol_* $1/
+mv dsigma_* $1/
+mv dsigma_* $1/
+mv *.res* $1/
+mv *.prm $1/
+mv *.log $1/
+
