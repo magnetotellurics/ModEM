3c7239d889d118ca92b45536a66ca8d643c74de8
Anna Kelbert <anna.kelbert@cfa.harvard.edu>
2023-09-18 22:03:09 +0000

    Reverting back the worker_job_task%solver_residual_vec diagnostics which, without proper initialization, can cause MPI segmentation faults depending on compiler specifics. Will add this back later with proper treatment in all the EMsolve3D.f90 modules and with precautions in Main_MPI.f90 even if this is not used.

--- f90/MPI/Declaration_MPI.f90
+++ f90/MPI/Declaration_MPI.f90
@@ -83,9 +83,6 @@ type :: define_worker_job
      logical       :: keep_E_soln=.false.
      logical       :: several_Tx=.false.
      logical       :: create_your_own_e0=.false.
-     DOUBLE PRECISION, pointer, dimension(:) :: solver_residual_vec
-     DOUBLE PRECISION    :: period
-     character*10  :: solver_name='QMR'
     ! 2022.10.06, Liu Zhongyin, add iSite storing the site index in rx of dataBlock_t
      Integer       :: iSite
  end type define_worker_job
@@ -99,18 +96,13 @@ Contains
 subroutine create_worker_job_task_place_holder
 
      implicit none
-     integer index,Nbytes1,Nbytes2,Nbytes3,Nbytes4,Nbytes5,Nbytes6,size_of_res_vector
-
-       size_of_res_vector=size(worker_job_task%solver_residual_vec)
+     integer index,Nbytes1,Nbytes2,Nbytes3
 
        CALL MPI_PACK_SIZE(80, MPI_CHARACTER, MPI_COMM_WORLD, Nbytes1,  ierr)
        CALL MPI_PACK_SIZE(9, MPI_INTEGER, MPI_COMM_WORLD, Nbytes2,  ierr)
        CALL MPI_PACK_SIZE(3, MPI_LOGICAL, MPI_COMM_WORLD, Nbytes3,  ierr)
-       CALL MPI_PACK_SIZE(size_of_res_vector, MPI_DOUBLE_PRECISION, MPI_COMM_WORLD, Nbytes4,  ierr)
-       CALL MPI_PACK_SIZE(1, MPI_DOUBLE_PRECISION, MPI_COMM_WORLD, Nbytes5,  ierr)
-       CALL MPI_PACK_SIZE(10, MPI_CHARACTER, MPI_COMM_WORLD, Nbytes6,  ierr)
 
-         Nbytes=(Nbytes1+Nbytes2+Nbytes3+Nbytes4+Nbytes5+Nbytes6)+1
+         Nbytes=(Nbytes1+Nbytes2+Nbytes3)+1
 
          if( associated( worker_job_package ) ) deallocate( worker_job_package )
          allocate( worker_job_package( Nbytes ) )
@@ -122,9 +114,6 @@ subroutine Pack_worker_job_task
      implicit none
      integer index,size_of_res_vector, ierr
 
-     index=1
-     size_of_res_vector=size(worker_job_task%solver_residual_vec)
-     
         call MPI_Pack(worker_job_task%what_to_do,80, MPI_CHARACTER, worker_job_package, Nbytes, index, MPI_COMM_WORLD, ierr)
 
         call MPI_Pack(worker_job_task%per_index ,1 , 		MPI_INTEGER  , worker_job_package, Nbytes, index, MPI_COMM_WORLD, ierr)
@@ -139,10 +128,6 @@ subroutine Pack_worker_job_task
         call MPI_Pack(worker_job_task%keep_E_soln,1, 		MPI_LOGICAL, worker_job_package, Nbytes, index, MPI_COMM_WORLD, ierr)
         call MPI_Pack(worker_job_task%several_Tx,1, 		MPI_LOGICAL, worker_job_package, Nbytes, index, MPI_COMM_WORLD, ierr)
         call MPI_Pack(worker_job_task%create_your_own_e0,1, 	MPI_LOGICAL, worker_job_package, Nbytes, index, MPI_COMM_WORLD, ierr)
-        call MPI_Pack(worker_job_task%solver_residual_vec(1), &
-                size_of_res_vector, MPI_DOUBLE_PRECISION, worker_job_package, Nbytes, index, MPI_COMM_WORLD, ierr)
-        call MPI_Pack(worker_job_task%period,1, 		MPI_DOUBLE_PRECISION, worker_job_package, Nbytes, index, MPI_COMM_WORLD, ierr)
-        call MPI_Pack(worker_job_task%solver_name,10,           MPI_CHARACTER, worker_job_package, Nbytes, index, MPI_COMM_WORLD, ierr)
 		
         ! 2019.05.08, Liu Zhongyin, add iSite for rx in dataBlock_t
         call MPI_Pack(worker_job_task%iSite, 1,                 MPI_INTEGER  , worker_job_package, Nbytes, index, MPI_COMM_WORLD, ierr)
@@ -152,9 +137,6 @@ end subroutine Pack_worker_job_task
 subroutine Unpack_worker_job_task
      implicit none
      integer index,size_of_res_vector, ierr
-
-     index=1
-     size_of_res_vector=size(worker_job_task%solver_residual_vec)
 
         call MPI_Unpack(worker_job_package, Nbytes, index, worker_job_task%what_to_do,80, MPI_CHARACTER,MPI_COMM_WORLD, ierr)
 
@@ -170,10 +152,6 @@ subroutine Unpack_worker_job_task
         call MPI_Unpack(worker_job_package, Nbytes, index, worker_job_task%keep_E_soln,1, MPI_LOGICAL,MPI_COMM_WORLD, ierr)
         call MPI_Unpack(worker_job_package, Nbytes, index, worker_job_task%several_Tx,1, MPI_LOGICAL,MPI_COMM_WORLD, ierr)
         call MPI_Unpack(worker_job_package, Nbytes, index, worker_job_task%create_your_own_e0,1, MPI_LOGICAL,MPI_COMM_WORLD, ierr)
-        call MPI_Unpack(worker_job_package, Nbytes, &
-                index, worker_job_task%solver_residual_vec(1),size_of_res_vector, MPI_DOUBLE_PRECISION,MPI_COMM_WORLD, ierr)
-        call MPI_Unpack(worker_job_package, Nbytes, index, worker_job_task%period,1, MPI_DOUBLE_PRECISION,MPI_COMM_WORLD, ierr)
-        call MPI_Unpack(worker_job_package, Nbytes, index, worker_job_task%solver_name,10, MPI_CHARACTER,MPI_COMM_WORLD, ierr)
 
         ! 2019.05.08, Liu Zhongyin, add iSite for rx in dataBlock_t
         call MPI_Unpack(worker_job_package, Nbytes, index, worker_job_task%iSite, 1, MPI_INTEGER,MPI_COMM_WORLD, ierr)
