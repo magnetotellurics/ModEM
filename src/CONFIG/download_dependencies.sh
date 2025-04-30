afc91d595d140185b18cd6552e7cb6b22415f6c4
Miles Curry <2590700+MiCurry@users.noreply.github.com>
2025-04-30 10:22:12 -0600

    Add a new Config file that works with intel and gfortran
    This commit adds a new Configuration file that works with either intel or
    gfortran. This is an attempt to reduce the amount of Configuration files down
    to 1.
    
    Mainly, this is an attempt to reduce the need to support or alter 10-15
    configuration files if an upgrade to the config files is needed. Although this
    version is on the CSEM branch, it will be a good template for something that
    the main branch can use.
    
    It requires a bit more from the user in certain situations. Specifically, it
    requires users to supply the FFTW directory if they compile the EM1D csem
    module. But it gives the user more flexibility, for instance they will be able
    to specify which compiler flags they want (by selecting gfortran or ifort), but
    they can use the FC environment variable to specify the name of their compiler
    (for instance, using FC=ftn for cray machines).
    
    The changes to this configuration file also remove the usages of `sed` in the
    configuration file. Previously, the configuration file would create a
    configuration file, and than alter it with `sed`. This makes the configuration
    file creation confusion and harder to support. This new configuration file
    builds up the `fmkmf.pl` options depending on the arguments. This should make
    it easier to maintain.
    
    To help with this transition, documentation will be added that walks users
    through such options.
    
    This configuration file also has capabilities to automatically obtain Dipole1D
    if it is requested and if it is not found within `3D_MT/CSEM_module/Dipole1d`
    directory.

---
+++ src/CONFIG/download_dependencies.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+OCCAM1DCSEM_ZIP_URL="https://marineemlab.ucsd.edu/Projects/Occam/1DCSEM/Occam1DCSEM_v3.13.zip"
+export DIPOLE1D_CSEM_MODEM_DIR="./3D_MT/CSEM_module/Dipole1D/"
+ZIP_FNAME="occam1dcsem.zip"
+OCCAM_DIR="Occam1DCSEM"
+
+cmd_exists()
+{
+    command -v "$1" >/dev/null 2>&1
+}
+
+get_dl_cmd() {
+    # Determine the download command, either Curl or Wget
+    cmd=""
+   
+    if cmd_exists wget; then
+        cmd="wget -O $ZIP_FNAME"
+        echo $cmd
+        return 0
+    fi
+ 
+    if cmd_exists curl; then
+        cmd="curl -o $ZIP_FNAME"
+        echo $cmd
+        return 0
+    fi
+
+    return 1
+}
+
+check_for_dipole1d() {
+    if [ -e $DIPOLE1D_CSEM_MODEM_DIR/Dipole1D.f90 ]; then
+        return 0
+    fi
+    return -1
+}
+
+
+download_dipole1d() {
+    force=${1:-0}
+
+    echo $DIPOLE1D_CSEM_MODEM_DIR
+    if [ ! -d $DIPOLE1D_CSEM_MODEM_DIR ]; then
+        echo "ERROR: We are not in the ./src directory of the CSEM branch could not find"
+        echo "ERROR: Please ensure we are in 'ModEM-Model/src' directory"
+        exit 1
+    fi
+
+    pushd $DIPOLE1D_CSEM_MODEM_DIR >/dev/null
+
+    if [ $(check_for_dipole1d) ] && [ $force -eq 0 ]; then
+        echo "Dipole1D already appears to be downloaded... skipping downloading"
+        echo "To re-download it remove the files within: $DIPOLE1D_CSEM_MODEM_DIR"
+        echo "Or pass 1 to this function: 'download_dipole1d 1'"
+        exit 0
+    fi
+
+    download_cmd=$(get_dl_cmd)
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Neither 'wget' or 'curl' are available on this system"
+        exit 1
+    fi
+
+    $download_cmd $OCCAM1DCSEM_ZIP_URL
+    if [ $? -ne 0 ]; then
+        popd >/dev/null
+        echo "ERROR: there was an error trying to download from $OCCAM1DCSEM_ZIP_URL"
+        exit 1
+    fi
+
+    unzip $ZIP_FNAME
+    if [ $? -ne 0 ]; then
+        popd >/dev/null
+        echo "ERROR: There was an error unzipping "
+        exit 1
+    fi
+
+    cp $OCCAM_DIR/Source/* .
+    rm -rf $OCCAM_DIR
+
+    popd >/dev/null
+}
+
+determine_if_dl_needed() {
+    if [ "$csem_type" == "Dipole1D" ] || [ "$csem_type" == "Dipole1D+EM1D" ]; then
+        # Check to see if we should download Dipole1D
+
+        check_for_dipole1d
+        dipole_present=$?
+        if [ $dipole_present != 0 ]; then
+            echo "Dipole1D is not currently in $DIPOLE1D_CSEM_MODEM_DIR"
+
+            while true; do
+                read -p "Would you like to have this script automatically download it now? [Yes/No]:" resp
+                resp_lower=$(to_lower resp)
+
+                if [ "$resp_lower" == "yes" ] || [ "$resp_lower" == "y" ]; then
+                    echo true
+                    return 0
+                elif [ "$resp_lower" == "no" ] || [ "$resp_lower" == "n" ]; then
+                    echo "ERROR: Asked for Dipole1D CSEM but Dipole1D not in: $DIPOLE1D_CSEM_MODEM_DIR"
+                    echo "ERROR: Either place Dipole1D into the directory above manually, or use this tool to download it"
+                    exit 1
+                fi
+            done
+        fi
+    fi
+    echo false
+    return 1
+}
+
+
